00aa9b431c6864e5c9061e867d25b54429e43e2a
liampc <kathliampcarlos@gmail.com>
2021-02-24 15:26:08 +0800

    add sass header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.scss';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+
+import './_header.scss'
+
+function Header(){
+
+
+    return (
+        <header className="header">
+            <div className="container">
+                <h1 className="header__title">Meal Planner</h1>
+            </div>
+            
+        </header>
+    )
+}
+
+export default Header
