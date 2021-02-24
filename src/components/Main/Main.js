cfd59de7fb666cbfe3b205d887212973ae8a44cc
liampc <kathliampcarlos@gmail.com>
2021-02-24 17:42:49 +0800

    edit datepicker style

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,10 +6,24 @@ import "react-datepicker/dist/react-datepicker.css";
 function Main(){
 
     const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
 
     return (
         <main className="container">
-            <DatePicker selected={startDate} onChange={date => setStartDate(date)}  dateFormat={'MMM dd, yyyy'}/>
+            <div className="date">
+                <div className="date__picker">
+                    <span></span>
+                    <DatePicker selected={startDate} onChange={date => setStartDate(date)}  dateFormat={'MMM dd, yyyy'}/>
+                    <span></span>
+                </div>
+                <div className="date__picker">
+                    <span></span>
+                    <DatePicker selected={endDate} onChange={date => setEndDate(date)}  dateFormat={'MMM dd, yyyy'}/>
+                    <span></span>
+                </div>
+            </div>
+          
+            
         </main>
     )
 }
